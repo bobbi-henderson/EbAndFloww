17676c26a2d01cbc019dc3aa807b5f12ed6fa1b1
Bobbi Henderson <bobbilhenderson@gmail.com>
2021-06-20 21:25:12 -0500

    added images to the art page with ability to toggle through images if more than one image

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,57 @@
-console.log('linked')
+let arrowRight = document.querySelector('.fa-arrow-right')
+let arrowLeft = document.querySelector('.fa-arrow-left')
+
+arrowRight.addEventListener('click', nextPicture)
+arrowLeft.addEventListener('click', lastPicture)
+
+function nextPicture() {
+    let images = document.querySelector('.artPics').children
+
+    for (let i=0; i<images.length; i++){
+        if(!images[i].classList.contains('d-none') && i!==images.length-1 && images.length !==2){
+            images[i].classList.add('d-none')
+            images[i+1].classList.remove('d-none')
+            break
+        } else if (!images[i].classList.contains('d-none') && images.length===2){
+            console.log('worked')
+            images[i].classList.add('d-none')
+            if(i==0){
+                images[1].classList.remove('d-none')
+            } else {
+                images[0].classList.remove('d-none')
+            }
+            break
+        } else if (!images[i].classList.contains('d-none')){
+            images[i].classList.add('d-none')
+            images[0].classList.remove('d-none')
+            break
+        } 
+    }
+
+}
+
+function lastPicture() {
+    let images = document.querySelector('.artPics').children
+
+    for (let i=0; i<images.length; i++){
+        if(!images[i].classList.contains('d-none') && i!==0 && images.length !==2){
+            images[i].classList.add('d-none')
+            images[i-1].classList.remove('d-none')
+            break
+        } else if (!images[i].classList.contains('d-none') && images.length===2){
+            console.log('worked')
+            images[i].classList.add('d-none')
+            if(i==0){
+                images[1].classList.remove('d-none')
+            } else {
+                images[0].classList.remove('d-none')
+            }
+            break
+        } else if (!images[i].classList.contains('d-none')){
+            images[i].classList.add('d-none')
+            images[i.length-1].classList.remove('d-none')
+            break
+        } 
+    }
+
+}
